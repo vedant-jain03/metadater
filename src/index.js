f8c8d943e0800f8c42e75085cae019acac3750c3
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2022-08-18 19:49:55 +0530

    added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-
+import { Route, Switch, Redirect } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/styles/tailwind.css";
 
@@ -17,7 +17,7 @@ import Profile from "views/Profile.js";
 import Index from "views/Index.js";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       {/* add routes with layouts */}
       <Route path="/admin" component={Admin} />
@@ -28,6 +28,6 @@ ReactDOM.render(
       {/* add redirect for first page */}
       <Redirect from="*" to="/" />
     </Switch>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
