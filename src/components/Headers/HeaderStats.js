f5c9c708fd0b7697e28f8a6411d6d041e718f348
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2022-08-26 04:51:17 +0530

    ux: graph design

--- src/components/Headers/HeaderStats.js
+++ src/components/Headers/HeaderStats.js
@@ -328,11 +328,10 @@ export default function HeaderStats() {
             </div>
           </div>
         </div>
-        <div className="px-4 md:px-10 mx-auto w-full mt-6">
-          <div>
-            {/* Card stats */}
-            <div className="flex flex-wrap">
-              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+        <div className='flex justify-center'>
+          <div className='px-4 md:pl-10 mx-auto mt-6 left_wrapper'>
+            <div className='flex flex-wrap'>
+              <div className="w-full lg:w-6/12 xl:w-6/12 px-4">
                 <CardStats
                   statSubtitle="Total Overloading"
                   statTitle="350,897"
@@ -344,7 +343,7 @@ export default function HeaderStats() {
                   statIconColor="bg-red-500"
                 />
               </div>
-              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+              <div className="w-full lg:w-6/12 xl:w-6/12 px-4">
                 <CardStats
                   statSubtitle="NEW USERS"
                   statTitle="2,356"
@@ -356,54 +355,56 @@ export default function HeaderStats() {
                   statIconColor="bg-orange-500"
                 />
               </div>
-              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
+            </div>
+            <div className='flex flex-wrap'>
+              <div className="w-full lg:w-6/12 xl:w-6/12 px-4 mt-4">
                 <CardStats
-                  statSubtitle="SALES"
-                  statTitle="924"
-                  statArrow="down"
-                  statPercent="1.10"
-                  statPercentColor="text-orange-500"
-                  statDescripiron="Since yesterday"
-                  statIconName="fas fa-users"
-                  statIconColor="bg-pink-500"
-                />
-              </div>
-              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
-                <CardStats
-                  statSubtitle="PERFORMANCE"
-                  statTitle="49,65%"
+                  statSubtitle="Total Overloading"
+                  statTitle="350,897"
                   statArrow="up"
-                  statPercent="12"
+                  statPercent="3.48"
                   statPercentColor="text-emerald-500"
                   statDescripiron="Since last month"
-                  statIconName="fas fa-percent"
-                  statIconColor="bg-lightBlue-500"
+                  statIconName="far fa-chart-bar"
+                  statIconColor="bg-red-500"
+                />
+              </div>
+              <div className="w-full lg:w-6/12 xl:w-6/12 px-4 mt-4">
+                <CardStats
+                  statSubtitle="NEW USERS"
+                  statTitle="2,356"
+                  statArrow="down"
+                  statPercent="3.48"
+                  statPercentColor="text-red-500"
+                  statDescripiron="Since last week"
+                  statIconName="fas fa-chart-pie"
+                  statIconColor="bg-orange-500"
                 />
               </div>
             </div>
           </div>
-        </div>
-        <div className='flex items-center graph_wrapper'>
-          <div className="rounded-t mb-0 px-4 py-3 border-0 graph_container">
-            <div className="flex flex-wrap items-center">
-              <div className="flex relative w-full px-4 max-w-full flex-grow flex-1">
-                <Line data={userData}
-                  options={{
-                    responsive: true,
-                    radius: 3,
-                    hitRadius: 5,
-                    hoverRadius: 5,
-                    plugins: {
-                      title: {
-                        display: true,
-                        text: 'Truck details'
+          <div className='flex items-center graph_wrapper'>
+            <div className="rounded-t mb-0 px-4 py-3 border-0 graph_container">
+              <div className="flex flex-wrap items-center">
+                <div className="flex relative w-full px-4 max-w-full flex-grow flex-1">
+                  <Line data={userData}
+                    options={{
+                      responsive: true,
+                      radius: 3,
+                      hitRadius: 5,
+                      hoverRadius: 5,
+                      plugins: {
+                        title: {
+                          display: true,
+                          text: 'Truck details'
+                        },
                       },
-                    },
-                    interaction: {
-                      intersect: true
-                    }
-                  }}
-                />
+                      interaction: {
+                        intersect: true
+                      }
+                    }}
+                  />
+                </div>
               </div>
             </div>
           </div>
