dce563d34719cf5215f4365b2512ae7060f28f12
mohitaiits <mohit.kadwe.cs19@ggits.net>
2022-08-25 23:04:19 +0530

    added chart in dashboard

--- src/components/Cards/CardTable.js
+++ src/components/Cards/CardTable.js
@@ -1,13 +1,68 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
 
 // components
 
 import TableDropdown from "components/Dropdowns/TableDropdown.js";
 
 
+const UserData = [
+  {
+    id: 1,
+    year: 2016,
+    userGain: 80000,
+    userLost: 823,
+  },
+  {
+    id: 2,
+    year: 2017,
+    userGain: 45677,
+    userLost: 345,
+  },
+  {
+    id: 3,
+    year: 2018,
+    userGain: 78888,
+    userLost: 555,
+  },
+  {
+    id: 4,
+    year: 2019,
+    userGain: 90000,
+    userLost: 4555,
+  },
+  {
+    id: 5,
+    year: 2020,
+    userGain: 4300,
+    userLost: 234,
+  },
+];
+
+
 
 export default function CardTable({ color }) {
+
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
 
   useEffect(() => {
     document.title = 'Dashboard';
@@ -55,6 +110,15 @@ export default function CardTable({ color }) {
           (color === "light" ? "bg-white" : "bg-lightBlue-900 text-white")
         }
       >
+
+        <div className="rounded-t mb-0 px-4 py-3 border-0">
+          <div className="flex flex-wrap items-center">
+            <div className="flex relative w-full px-4 max-w-full flex-grow flex-1">
+              <Line data={userData} />
+            </div>
+          </div>
+        </div>
+
         <div className="rounded-t mb-0 px-4 py-3 border-0">
           <div className="flex flex-wrap items-center">
             <div className="flex relative w-full px-4 max-w-full flex-grow flex-1">
@@ -66,10 +130,12 @@ export default function CardTable({ color }) {
               >
                 Truck Details
               </h3>
-              <i className="fas fa-redo ml-2 text-lg text-white-400 cursor-pointer" onClick={()=>{window.location.reload();}}></i>
+              <i className="fas fa-redo ml-2 text-lg text-white-400 cursor-pointer" onClick={() => { window.location.reload(); }}></i>
+
             </div>
           </div>
         </div>
+
         <div className="block w-full overflow-x-auto">
           {/* Projects table */}
           <table className="items-center w-full bg-transparent border-collapse">
